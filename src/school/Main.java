f2fb2039ed14f473a2fc795944db2e0a3493a9d8
Vitvarova-J-31c9 <Vitvarova-J-31c9@a2-01-mti.skynet.tul.cz>
2024-09-25 08:23:54 +0200

    Add Hello world

--- src/school/Main.java
+++ src/school/Main.java
@@ -3,7 +3,7 @@ package school;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
