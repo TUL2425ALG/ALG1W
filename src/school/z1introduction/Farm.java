f6f8dc6cb1727800ad1a9ef57a15e9de58b42e56
Jana Vitvar <jana.vitvarova@tul.cz>
2024-09-25 10:22:30 +0200

    add sentence add farm

---
+++ src/school/z1introduction/Farm.java
@@ -0,0 +1,24 @@
+
+package school.z1introduction;
+
+public class Farm {
+    public static void main(String[] args) {
+        
+    
+    //input
+    int pocetHus = 5;
+    int pocetKraliku = 4;
+    
+    //processing
+    int pocetZvirat = pocetKraliku + pocetHus;
+    int pocetNohou = pocetKraliku * 4 + pocetHus * 2;
+   
+    //output
+    System.out.println("Pocet kraliku: " + pocetKraliku);
+    System.out.println("Pocet hus: " + pocetHus);
+    System.out.println("Pocet zvirat: " + pocetZvirat);
+    System.out.println("Pocet nohou: " + pocetNohou);
+
+    }
+}
+    
