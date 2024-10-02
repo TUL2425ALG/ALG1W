d096d5b91f00d49a1c90c2d26769a7b1f19cdc3f
Jana Vitvar <jana.vitvarova@tul.cz>
2024-10-02 08:49:17 +0200

    add circle calculations

---
+++ src/school/z2/numbers/U6Circle.java
@@ -0,0 +1,25 @@
+
+package school.z2.numbers;
+
+import java.util.Scanner;
+
+
+public class U6Circle {
+    public static void main(String[] args) {
+    //input
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Zadej polomer:");
+    double r = sc.nextDouble();//2;
+    //final double PI = Math.PI;//3.14159;
+    
+    
+    //processing
+    double obsah = Math.PI * Math.pow(r, 2); //polomer * polomer;
+    double obvod = 2 * Math.PI * r;
+    
+    //output
+    //System.out.println("Obsah rovna se: " + obsah + ",\nObvod rovna se: "+ obvod + ".");
+    System.out.format("Obsah rovna se: %.2f, \nObvod rovna se: %.2f.\n", obsah, obvod);
+    
+    }
+}
