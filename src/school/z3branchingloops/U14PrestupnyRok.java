24481749886e5475cdf2fe67d4f58fc36ac51ec0
Jana Vitvar <jana.vitvarova@tul.cz>
2024-10-16 10:02:23 +0200

    methods

--- src/school/z3branchingloops/U14PrestupnyRok.java
+++ src/school/z3branchingloops/U14PrestupnyRok.java
@@ -14,7 +14,27 @@ public class U14PrestupnyRok {
     public static void main(String[] args) {
         int rok = 2001;
         
-        System.out.println(((rok % 4) == 0) && (!((rok % 100) == 0) || ((rok % 400) == 0)) ? 366 : 365);
+//        boolean isLeap = ((rok % 4) == 0 && !((rok % 100) == 0)) || (rok % 400) == 0;
+//        int nDays = isLeap? 366 : 365;
+        
+//        int nDays = isLeap(rok)?366 : 365;
+           int nDays = getNumberDays(rok); 
+        
+        System.out.println(nDays);
     }
+    /**
+     * Returns whether it is a specified year is leap
+     * @param year
+     * @return 
+     */
+    public static boolean isLeap(int year){
+        boolean isLeap = ((year % 4) == 0 && !((year % 100) == 0)) || (year % 400) == 0;
+        return isLeap;
+    }
+    
+    public static int getNumberDays(int year){
+        return isLeap(year)? 366 : 365;
+    }
+    
     
 }
