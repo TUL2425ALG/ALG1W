670964cc78d17bf917e129abdab3af6d76eeca92
vzadinaaa <vaclav.zadina@tul.cz>
2024-12-10 20:56:52 +0100

    Add files via upload
    add check valid RC

---
+++ src/school/z3branchingloops/birthNumber.java
@@ -0,0 +1,33 @@
+package z3branchingloops;
+
+public class birthNumber {
+
+    public static void main(String[] args) {
+        String firstPart = "780123";
+        String secondPart = "3540";
+
+        String fullNumber = firstPart + secondPart;
+
+        long firstNineNumbers = Long.valueOf(fullNumber.substring(0, 9));
+
+        long remainder = firstNineNumbers % 11;
+        long lastDigit = Long.valueOf(fullNumber) % 10;
+
+        boolean valid;
+
+        if ((remainder == 10) && (lastDigit == 0)) 
+            valid = true;
+         else if (remainder == lastDigit) 
+            valid = true;
+         else 
+            valid = false;
+        
+
+        if (valid) 
+            System.out.println("Rodné číslo je platné.");
+         else 
+            System.out.println("Rodné číslo není platné.");
+        
+
+    }
+}
