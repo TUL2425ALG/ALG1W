c021b351856a7630102df69259ee4c20bab9af69
Tomáš Vostiňák <tomas.vostinak@tul.cz>
2024-12-11 18:43:11 +0100

    Update U4StochastickaMatice

--- src/school/z7array2d/U4StochastickaMatice.java
+++ src/school/z7array2d/U4StochastickaMatice.java
@@ -5,24 +5,23 @@ public class U4StochastickaMatice {
         double[][] a = {
             {1, 0, 0},
             {0, 1, 0},
-            {0, 0, 1}
+            {0, 0, 0.5}
         };
         System.out.println(stochastickaMatice(a));
     }
     
-    //JV prochazejte sloupce a u kazdeho prvku testujte jestli je zaporny, pokud ano false, zaroven delejte soucet na radku a po kazdem radku otestujte sumu na 1
     public static boolean stochastickaMatice(double[][] matice){
         for(int i = 0; i < matice.length; i++){
             double soucet = 0;
             for(int j = 0; j < matice[i].length; j++){
+                if(matice[i][j] < 0 || matice[i][j] > 1){
+                    return false;
+                }
                 soucet += matice[i][j];
             }
-            for(int j = 0; j < matice[i].length; j++){
-                if ((matice[i][j] > 1 || matice[i][j] < 0) || soucet != 1){ //JV je zbytecne pro kazdy prvek na radku testovat zda suma radku nerovna se 1
-                    return false;                                           
-                }
+            if(soucet != 1){
+                return false;
             }
-            soucet = 0;
         }
         return true; 
     }
